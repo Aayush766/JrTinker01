0db763796229584a486ea7010b2fc6959dfa3b9c
Aayush766 <blaze.aayush23@gmail.com>
2026-02-19 12:09:28 +0530

    health checksss

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userModel = require("../models/User");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 
 const RegisterUser = async (req, res) => {
